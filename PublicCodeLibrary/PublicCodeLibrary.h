61aa422b82084e2b02740059570bd38093ae1c44
blackjacx <stefan.herold@gmail.com>
2013-05-16 17:02:47 +0200

    discareded the framework approach and switched back building a static lib

--- PublicCodeLibrary/PublicCodeLibrary.h
+++ PublicCodeLibrary/PublicCodeLibrary.h
@@ -1,38 +1,13 @@
-/*!
- @file		PublicCodeLibrary.h
- @brief		The global header containing all sub headers.
- @details	This header file can be imported in the PCH of projects that make 
-			use of this library with the effect to have all the useful stuff 
-			at hand ... everywhere.
- @author	Stefan Herold
- @date		2011-10-24
- @copyright	Copyright (c) 2012 Stefan Herold. All rights reserved.
- */
+//
+//  PublicCodeLibrary.h
+//  PublicCodeLibrary
+//
+//  Created by Stefan Herold on 16.05.13.
+//  Copyright (c) 2013 Stefan Herold. All rights reserved.
+//
 
-// MARK: Core
-#import <PublicCodeLibrary/PCLDebugging.h>
-#import <PublicCodeLibrary/PCLStrings.h>
-#import <PublicCodeLibrary/PCLTypeDefs.h>
+#import <Foundation/Foundation.h>
 
-// MARK: Foundation Additions
-#import <PublicCodeLibrary/NSArray+PCLExtensions.h>
-#import <PublicCodeLibrary/NSData+PCLExtensions.h>
-#import <PublicCodeLibrary/NSDate+PCLExtensions.h>
-#import <PublicCodeLibrary/NSError+PCLExtensions.h>
-#import <PublicCodeLibrary/NSNotificationCenter+PCLExtensions.h>
-#import <PublicCodeLibrary/NSObject+PCLExtensions.h>
-#import <PublicCodeLibrary/NSString+PCLExtensions.h>
-#import <PublicCodeLibrary/NSURL+PCLExtensions.h>
-#import <PublicCodeLibrary/NSURLRequest+PCLExtensions.h>
-#import <PublicCodeLibrary/PCLBackgroundTaskHandler.h>
+@interface PublicCodeLibrary : NSObject
 
-// MARK: UIKit Extensions
-#import <PublicCodeLibrary/UIApplication+PCLExtensions.h>
-#import <PublicCodeLibrary/UIColor+PCLExtensions.h>
-#import <PublicCodeLibrary/UIDevice+PCLExtensions.h>
-#import <PublicCodeLibrary/UIImage+PCLExtensions.h>
-#import <PublicCodeLibrary/UIView+PCLExtensions.h>
-#import <PublicCodeLibrary/UIViewConroller+PCLExtensions.h>
-
-// MARK: Backend
-#import <PublicCodeLibrary/PCLRestCommand.h>
+@end
